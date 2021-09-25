f4879c6b04ad86b1640fa1bd144abcd8c14cdfd4
Aastha Pandey <aasthap7@gmail.com>
2021-09-25 14:22:53 +0000

    first commit

---
+++ precache-manifest.9622798ac4054981f4080dcb7082bf1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9fa443033fce0fa2e92ae7617bd24f",
+    "url": "/eat-in/index.html"
+  },
+  {
+    "revision": "b5b1af28f0cfd9e6d7f4",
+    "url": "/eat-in/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "0ff5b6eaf3b3654af2c8",
+    "url": "/eat-in/static/css/main.b923e118.chunk.css"
+  },
+  {
+    "revision": "b5b1af28f0cfd9e6d7f4",
+    "url": "/eat-in/static/js/2.2391098e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eat-in/static/js/2.2391098e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff5b6eaf3b3654af2c8",
+    "url": "/eat-in/static/js/main.1ac70ba0.chunk.js"
+  },
+  {
+    "revision": "b87377e7afb3196fa75e",
+    "url": "/eat-in/static/js/runtime-main.6ad072be.js"
+  },
+  {
+    "revision": "ad3daeceab4732526b1b316bd342c21b",
+    "url": "/eat-in/static/media/logo.ad3daece.svg"
+  }
+]);
